6550b69d95f876a7a9e74e084be0db1457628a67
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-09-10 14:20:34 -0500

    add keg details function componenet

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -6,10 +6,10 @@ function KegDetail(props){
   const { keg } = props;
   return(
     <React.Fragment>
-      <h5>{props.name} Details:</h5>
-      <h5>{props.brand}</h5>
-      <h5>{props.price}</h5>
-      <h5>{props.flavor}</h5>
+      <h5>{keg.name} Details:</h5>
+      <h5>{keg.brand}</h5>
+      <h5>{keg.price}</h5>
+      <h5>{keg.flavor}</h5>
       <hr/>
     </React.Fragment>
   );
