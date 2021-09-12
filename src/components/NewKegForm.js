4b3c5d88ee8530ff9957b3a87257750df090727c
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-09-12 12:38:23 -0500

    add reusable form component

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,40 +1,16 @@
 import React from "react";
 import {v4} from "uuid";
 import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
 
 function NewKegForm(props){
-
   function handleNewKegFormSubmission(event){
     event.preventDefault();
     props.onNewKegCreation({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, flavor: event.target.flavor.value , pints: 124, id: v4()});
     }
-
-
   return(
     <React.Fragment>
-      <form onSubmit ={handleNewKegFormSubmission}>
-        <input
-          type='text'
-          name='name'
-          placeholder='Name'
-          />
-          <input
-          type='text'
-          name='brand'
-          placeholder='Brand'
-          />
-          <input
-          type='text'
-          name='price'
-          placeholder= 'Price'
-          />
-          <input
-          type='text'
-          name='flavor'
-          placeholder= 'Flavor'
-          />
-          <button type='submit'>Add!</button>
-      </form>
+      <ReusableForm formSubmissionHandler={handleNewKegFormSubmission} buttonText="Add!" />
     </React.Fragment>
   );
 }
